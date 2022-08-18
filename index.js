0b7b424db4f89f48e9cecd0b6dcfcccf164bc268
junhyeong <ljh040325@gmail.com>
2022-08-18 16:27:10 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ const encoder = new util.TextEncoder('utf-8');
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ljh040325:jh9171w!!@youtube.t3qdvoy.mongodb.net/?retryWrites=true&w=majority')
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI)
 .then(() => console.log("MongoDB Connectred..."))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => res.send('hello new World!!'))
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false , err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
